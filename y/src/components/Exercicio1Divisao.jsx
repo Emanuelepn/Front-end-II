36c8e51e29d3614a9cfcec4f085c6d9689debc83
Emanuelepn <emanuel.epnb@gmail.com>
2024-03-20 22:21:55 -0300

    primeiro commit

---
+++ y/src/components/Exercicio1Divisao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exercicio1Divisao = (props) => {
+  return (
+    <div>
+      O resultado da divisão de num1 por num2 é: {props.num1 / props.num2}
+    </div>
+  )
+}
+
+export default Exercicio1Divisao
---
+++ y/src/components/Exercicio1Multiplicacao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exercicio1Multiplicacao = (props) => {
+  return (
+    <div>
+      O resultado da multiplicação de num1 e num2 é:{props.num1 * props.num2}
+    </div>
+  )
+}
+
+export default Exercicio1Multiplicacao
